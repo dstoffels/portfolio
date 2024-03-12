cd99723eb7f9c10781d37654396f010a56e43de6
Danimal <dan.stoffels@gmail.com>
2024-03-12 00:49:49 -0500

    fix: update base page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import BotForm from '@/components/bot-form';
 import { PropsWithChildren } from 'react';
 
-export type PageProps = PropsWithChildren & {};
-
-export default function Page({}: PageProps) {
+export default function Page({}) {
 	return <div>Main Page</div>;
 }
