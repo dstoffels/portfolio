6050102184b7db6f99026cd99968c22eff3b93f9
Danimal <dan.stoffels@gmail.com>
2024-03-14 10:07:20 -0500

    refactor: retrieval files for assistants api

--- components/BotTextArea.tsx
+++ components/BotTextArea.tsx
@@ -1,36 +1,42 @@
 'use client';
+import { useFormState, useFormStatus } from 'react-dom';
 import { BsArrowUpSquareFill } from 'react-icons/bs';
 
-const BotTextArea = () => {
+const BotForm = ({ onSubmit }: { onSubmit: (formData: FormData) => Promise<void> }) => {
+	const { pending, method } = useFormStatus();
+
 	const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
 		if (e.key === 'Enter' && !e.shiftKey) {
 			e.preventDefault();
 			const form = document.getElementById('bot-form') as HTMLFormElement;
-			form?.requestSubmit();
+			form.requestSubmit();
 		}
 	};
 
 	return (
-		<div className="border-zinc-600 border rounded-2xl border-solid px-4 py-3 flex">
-			<textarea
-				rows={1}
-				name="content"
-				onChange={(e) => autoExpand(e.target)}
-				onKeyDown={handleKeyDown}
-				className="w-full bg-transparent focus:outline-none resize-none placeholder:text-zinc-500"
-				placeholder="Ask me anything!"
-			/>
-			<button type="submit">
-				<BsArrowUpSquareFill />
-			</button>
-		</div>
+		<form id="bot-form" action={onSubmit} className="px-36 h-24 bg-zinc-800 fixed w-full bottom-0">
+			<div className="border-zinc-600 border rounded-2xl border-solid px-4 py-3 flex">
+				<textarea
+					rows={1}
+					name="content"
+					onChange={(e) => autoExpand(e.target)}
+					onKeyDown={handleKeyDown}
+					className="w-full bg-transparent focus:outline-none resize-none placeholder:text-zinc-500"
+					placeholder="Ask me anything!"
+				/>
+				<button type="submit" aria-disabled={pending}>
+					<BsArrowUpSquareFill size={25} className={pending ? 'text-zinc-700' : 'text-inherit'} />
+				</button>
+			</div>
+		</form>
 	);
 };
 
-export default BotTextArea;
+export default BotForm;
 function autoExpand(textarea: HTMLTextAreaElement) {
 	textarea.style.height = 'auto';
 	textarea.style.height = textarea.scrollHeight + 'px';
 }
 
 ('block w-full rounded-md border-0 py-1.5 px-5 text-white-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-black');
+('- Only reference my personal information from retrieval files provided.');
--- components/header.tsx
+++ components/header.tsx
@@ -2,9 +2,10 @@ import NavLink from './NavLink';
 
 const Header = () => {
 	return (
-		<header className="px-2 py-2 dark:bg-zinc-900 w-1/6">
-			<h1 className="font-bold text-lg">Dan O Stoffels</h1>
-			<NavLink label="ChatBot" />
+		<header className="px-36 h-16 dark:bg-zinc-900 fixed top-0 w-full flex gap-3 items-center">
+			<h1 className="font-bold text-lg flex-1">Dan O Stoffels</h1>
+			<NavLink label="ChatBot" href="/bot" />
+			<NavLink label="Curriculum Vitae" href="/cv" />
 		</header>
 	);
 };
