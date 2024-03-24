56f790e28c3eacf732263cf7ec81e7123ed2aee4
Danimal <dan.stoffels@gmail.com>
2024-03-24 02:48:11 -0500

    fix: data fetching

--- app/cv/actions.ts
+++ app/cv/actions.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { CVData } from '@/app/types';
 
 export function fetchCVData() {
-	const cvFile = fs.readFileSync('./data/cv.yaml', 'utf-8');
+	const cvFile = fs.readFileSync(`data/cv.yaml`, 'utf-8');
 	const data = yaml.parse(cvFile, { toStringDefaults: {} }) as CVData;
 
 	return data;
